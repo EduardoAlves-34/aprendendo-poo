ad967e153dc658ded32ead4ddb6c5300ba8db268
Eduardo <eduardoalves3456@hotmail.com>
2023-05-25 18:15:32 -0300

    alteração de variavel e criação de uma nova conta

--- src/main/java/com/brs/brsaplication/BrsaplicationApplication.java
+++ src/main/java/com/brs/brsaplication/BrsaplicationApplication.java
@@ -10,26 +10,31 @@ public class BrsaplicationApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(BrsaplicationApplication.class, args);
 
-		ContaBanco Pedrin = new ContaBanco();
-		ContaBanco brunao = new ContaBanco();
+		ContaBanco p1 = new ContaBanco();
+		ContaBanco p2 = new ContaBanco();
+		ContaBanco p3 = new ContaBanco();
+
+		p1.setUser("Bruno");
+		p1.setAccountNum(15);
+		p1.setBalance(1000.00);
+		p1.setAccountType("cc");
+		p1.setStats(false);
+
+		p2.setUser("Pedro");
+		p2.setAccountNum(17);
+		p2.setBalance(2000.00);
+		p2.setAccountType("cp");
+		p2.setStats(false);
+
+		p3.setUser("Fabricio");
+		p3.setAccountNum(20);
+		p3.setBalance(3000.00);
+		p3.setAccountType("cc");
+		p3.setStats(false);
 
 
-		brunao.setUser("Brunão");
-		brunao.setAccountNum(15);
-		brunao.setBalance(1000.00);
-		brunao.setAccountType("cc");
-		brunao.setStats(false);
 
-		Pedrin.setUser("Pedrinho");
-		Pedrin.setAccountNum(17);
-		Pedrin.setBalance(2000.00);
-		Pedrin.setAccountType("cp");
-		Pedrin.setStats(false);
-
-		Pedrin.openNewAccount();
-		Pedrin.payTuition();
-
-		System.out.println(Pedrin.getBalance());
+		System.out.println(p3.getBalance());
 
 	}
 }
