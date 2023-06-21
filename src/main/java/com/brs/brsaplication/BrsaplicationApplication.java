06c99ab081948a0c464bd93cb395821e10a67694
EduardoAlves-34 <104652577+EduardoAlves-34@users.noreply.github.com>
2023-06-21 16:34:43 -0300

    Update BrsaplicationApplication.java

--- src/main/java/com/brs/brsaplication/BrsaplicationApplication.java
+++ src/main/java/com/brs/brsaplication/BrsaplicationApplication.java
@@ -10,28 +10,7 @@ import com.brs.brsaplication.encapsulamento.ControlImpl;
 public class BrsaplicationApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(BrsaplicationApplication.class, args);
-
-		Character P[] = new Character[5];
-		Fight UFC = new Fight();
-
-		P[0] = new Character("Ciri","brazil","female",20,1.70,75.00,100,0,0);
-
-		P[1] = new Character("Yennefer","United States","female" ,28,1.71,77.00,50,0,10);
-
-		UFC.brandFight(P[0],P[1]);
-		UFC.toFight();
-
-		P[0].status();
-
-		P[1].status();
-
-
-
-
-
-
-
+		SpringApplication.run(BrsaplicationApplication.class, args);
 
 	}
 }
